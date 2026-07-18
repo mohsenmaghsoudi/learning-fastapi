e71200ce2ea36539731f44f60566f26dc1e34bbc
Mohsen Maghsoudi <mohsen.maghsoudi1911@gmail.com>
2026-07-18 23:32:05 +0330

    about path param,query param amd priority path,pydantic,query

--- main.py
+++ main.py
@@ -28,3 +28,29 @@ def create_posts(new_post: Post):
     print(new_post.rating)
     print(new_post.model_dump())  # convert to dictionary
     return {"data": new_post}
+
+
+# priority of path param, pydantic param, query param
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+#
+# app = FastAPI()
+#
+# # مدل Pydantic
+# class Book(BaseModel):
+#     title: str
+#     author: str
+#     rating: int
+#
+# @app.put("/books/{book_id}")
+# async def update_book(
+#     book_id: int,          # 1. Path Parameter (اجباری - بدون پیش‌فرض)
+#     book_data: Book,       # 2. Pydantic Model (اجباری - بدون پیش‌فرض)
+#     q: str | None = None   # 3. Query Parameter (اختیاری - با پیش‌فرض None)
+# ):
+#     result = {"book_id": book_id, **book_data.model_dump()}
+#
+#     if q:
+#         result.update({"q": q})
+#
